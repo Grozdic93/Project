e006d91d7f8263a0f85b1df5b3da334f7a89225b
abdalhamedthaljy <59847991+abdalhamedthaljy@users.noreply.github.com>
2023-02-27 12:10:36 +0100

    add footer -add details comp

---
+++ src/app/street-food-details/street-food-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-street-food-details',
+  templateUrl: './street-food-details.component.html',
+  styleUrls: ['./street-food-details.component.css']
+})
+export class StreetFoodDetailsComponent {
+
+}
