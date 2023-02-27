50f1a8881f228b227e8593cc8551091deb8ef545
abdalhamedthaljy <59847991+abdalhamedthaljy@users.noreply.github.com>
2023-02-27 14:07:06 +0100

    add font link in index html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LightgalleryModule } from 'lightgallery/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -20,6 +20,7 @@ import { HeroGalleryComponent } from './hero-gallery/hero-gallery.component';
 @NgModule({
   declarations: [
     AppComponent,
+    
     FooterComponent,
     NavbarComponent,
     HeroSectionComponent,
@@ -36,7 +37,9 @@ import { HeroGalleryComponent } from './hero-gallery/hero-gallery.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   LightgalleryModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
