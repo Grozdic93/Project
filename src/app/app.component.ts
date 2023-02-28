68aa566c1c38247a9a0454a28f5e9a180e483987
Grozdic93 <dusgrozdic@gmail.com>
2023-02-28 13:58:30 +0100

    scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Project';
 
+  onActivate(event) {
+    // window.scroll(0,0);
+ 
+    window.scroll({ 
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+     });
+ 
+     //or document.body.scrollTop = 0;
+     //or document.querySelector('body').scrollTo(0,0)
+     
+ }
 }
