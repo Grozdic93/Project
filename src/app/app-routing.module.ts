3b5c5762d0e3aa85c219306be67b4e10715959f1
Grozdic93 <dusgrozdic@gmail.com>
2023-02-28 10:01:14 +0100

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { AboutPageComponent } from './about-page/about-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { CitiesDetailsComponent } from './cities-details/cities-details.component';
+import { ArchitecturalDetailsComponent } from './architectural-details/architectural-details.component';
+import { LandScapesDetailsComponent } from './land-scapes-details/land-scapes-details.component';
+import { WildLifeDetailsComponent } from './wild-life-details/wild-life-details.component';
+import { PeopleDetailsComponent } from './people-details/people-details.component';
+import { StreetFoodDetailsComponent } from './street-food-details/street-food-details.component';
 const routes: Routes = [
   {path:"", component: HomePageComponent
 },{
@@ -11,7 +16,17 @@ const routes: Routes = [
   },{
     path:"gallery", component: GalleryComponent
   },{
-    path:"details", component: CitiesDetailsComponent
+    path:"gallery/cities-details", component: CitiesDetailsComponent
+  },{
+    path:"gallery/architectural-details", component: ArchitecturalDetailsComponent
+  },{
+    path:"gallery/landscapes-details", component: LandScapesDetailsComponent
+  },{
+    path:"gallery/wildelife-details", component: WildLifeDetailsComponent
+  },{
+    path:"gallery/people-details", component: PeopleDetailsComponent
+  },{
+    path:"gallery/streetfood-details", component: StreetFoodDetailsComponent
   }];
 
 
