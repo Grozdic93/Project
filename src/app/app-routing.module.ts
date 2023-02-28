568a096abeb115555d2fa52d43211d656886d626
Grozdic93 <dusgrozdic@gmail.com>
2023-02-28 01:54:41 +0100

    mes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { GalleryComponent } from './gallery/gallery.component';
+const routes: Routes = [
+  {path:"", component: HomePageComponent
+},{
+    path:"about", component: AboutPageComponent
+  },{
+    path:"gallery", component: GalleryComponent
+  }];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
