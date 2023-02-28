4780a25516ccb8b7a610efcffa472de9c9a9b0c4
Shams <shams.faqiri1996@gmail.com>
2023-02-28 12:06:33 +0100

    changes

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-about-page',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AboutPageComponent {
 
+  confirmAction(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Send it!'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Send!',
+          'Your file has been Send.',
+          'success'
+        )
+      }
+    })
+  }
+  
 }
+
+
+
+
