a2fafa058367c32dad202b8340c6cd9d1474ff22
Grozdic93 <dusgrozdic@gmail.com>
2023-02-28 09:04:26 +0100

    route details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { CitiesDetailsComponent } from './cities-details/cities-details.component';
 const routes: Routes = [
   {path:"", component: HomePageComponent
 },{
     path:"about", component: AboutPageComponent
   },{
     path:"gallery", component: GalleryComponent
+  },{
+    path:"details", component: CitiesDetailsComponent
   }];
 
 
