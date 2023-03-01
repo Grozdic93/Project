b9e947f769aa93d0edd6074fb33cfadc24fedbe5
DonatellaTesei <donatella.tesei@gmail.com>
2023-03-01 13:52:09 +0100

    home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { prints} from '../prints';
 import { Iprint } from '../Iprint';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
 prints: Array<Iprint> = prints;
 available: boolean = true;
 print: Iprint = {} as Iprint;
 id: number = 0;
 
-constructor(private route: ActivatedRoute,
-  private router: Router) { }
+ngOnInit(){
+  // this.router.navigate(["/"]);
+}
 
-toBottom(){window.scrollTo(0,2400)
+constructor(private route: ActivatedRoute,
+  private router: Router, private scroller: ViewportScroller) { }
+
+toBottom(){
+  // window.scrollTo(0,2200)
+  this.scroller.scrollToAnchor("prints");
 }
 
 }
